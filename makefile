10ac74e434efb200d659dea7d98b9504e7c7dc6c
Partho Sarthi <psarthi16@gmail.com>
2018-02-21 16:39:51 +0530

    finished first and follow sets

--- makefile
+++ makefile
@@ -1,16 +1,15 @@
-all: clean release
 
-release:
+all: clean debug
+
+debug:
 	gcc src/lexer.c -c -o out/lexer.o 
 	gcc src/driver.c -c -o out/driver.o
-	gcc -o build/pcc out/lexer.o out/driver.o
+	gcc -o build/pcc out/lexer.o out/driver.o	
 
 firstfollow:
-	gcc src/firstAndFollow.c -o build/firstAndFollow
+	gcc src/firstAndFollow.c -g -o build/firstAndFollow
 
 
 clean:
 	rm -f build/* out/*
 
-run:
-	./build/pcc $(file)
--- src/firstAndFollow.c
+++ src/firstAndFollow.c
@@ -28,6 +28,7 @@ typedef struct SymbolList SymbolList;
 struct SymbolList {
 	SymbolDef * symbol;
 	struct SymbolList *next;
+	struct SymbolList *prev;
 };
 
 bool isEpsilon(SymbolDef * symbol){
@@ -69,6 +70,7 @@ SymbolDef * insertSymbol(SymbolList ** symbols,SymbolDef *new_symbol){
 	SymbolList *last = *symbols;
 	new_symbol_ls->symbol = new_symbol;
 	new_symbol_ls->next = NULL;
+	new_symbol_ls->prev = NULL;
 
 	if (*symbols == NULL)
     {
@@ -81,6 +83,7 @@ SymbolDef * insertSymbol(SymbolList ** symbols,SymbolDef *new_symbol){
         last = last->next;
     }
     last->next = new_symbol_ls;
+    new_symbol_ls->prev = last;
 	return new_symbol;
 }
 
@@ -129,29 +132,6 @@ void insertRule(SymbolDef **symbol,char * rightSide){
     last->next = new_rule;
 }
 
-void viewSymbols(){
-	SymbolList * temp = symbols;
-	while(temp!=NULL){
-		if(temp->symbol->isTerminal==0){
-			printf("%s->",temp->symbol->value);
-			RuleList * temp2 = temp->symbol->rules;
-			while(temp2!=NULL){
-				printf("{");
-				SymbolList * temp3 = temp2->rule_tokens;
-				while(temp3!=NULL){
-					printf("%s,",temp3->symbol->value);
-					temp3 = temp3->next;
-				}
-				printf("},");
-				temp2 = temp2->next;
-			}
-			printf("\n");
-		}
-		temp = temp->next;
-	}
-}
-
-
 
 void parseGrammar(char const *f_name){
 	FILE * fp;
@@ -179,17 +159,35 @@ void parseGrammar(char const *f_name){
 }
 
 
-void mergeList(SymbolList ** first_list, SymbolList * first){
-	SymbolList * temp = first;
+int sizeOfList(SymbolList * symbols){
+	SymbolList * temp = symbols;
+	int count=0;
 	while(temp!=NULL){
+		count++;
+		temp=temp->next;
+	}
+	return count;
+}
+
+int mergeList(SymbolList ** first_list, SymbolList * first,int includeEps){
+	SymbolList * temp = first;
+	int init_sz = sizeOfList(*first_list);
+	while(temp!=NULL){
+		if(includeEps == 0 && isEpsilon(temp->symbol)){
+			temp=temp->next;
+			continue;
+		}
 		if(getSymbolIndex(first_list,temp->symbol->value)==NULL)
 			insertSymbol(first_list,temp->symbol);
 		temp=temp->next;
 	}
+	int final_sz = sizeOfList(*first_list);
+	return final_sz != init_sz;
 }
 
 bool hasEpsilon(SymbolList * symbols){
 	SymbolList * temp = symbols;
+	
 	while(temp!=NULL){
 		if(isEpsilon(temp->symbol)){
 			return true;
@@ -199,80 +197,120 @@ bool hasEpsilon(SymbolList * symbols){
 	return false;
 }
 
-SymbolList * findFirst_R(SymbolDef *symbol){
-	if(symbol->first!=NULL){
-		return symbol->first;
-	}
-	else{
-		SymbolList *first_list = NULL;
-	
-		if(symbol->isTerminal==1){
-			insertSymbol(&first_list,symbol);
-			return first_list;
-		}
-
-		RuleList * temp2 = symbol->rules;
-		while(temp2!=NULL){
-			SymbolList * temp3 = temp2->rule_tokens;
-			SymbolList * first = findFirst_R(temp3->symbol);
-			if(temp3->symbol->first==NULL)
-				mergeList(&temp3->symbol->first,first);
-			mergeList(&first_list,first);
-			while(hasEpsilon(first)){
-				temp3 = temp3->next;
-				if(temp3==NULL)
-					break;
-				SymbolList * first = findFirst_R(temp3->symbol);
-				if(temp3->symbol->first==NULL)
-					mergeList(&temp3->symbol->first,first);
-				mergeList(&first_list,first);
+int allEpsilon(SymbolList * begin,SymbolList * end){
+	SymbolList * temp = begin;
+	int flg;
+	while(temp!=end){
+		flg = 0;
+		if(temp->symbol->isTerminal==0){
+			RuleList * temp2 = temp->symbol->rules;
+			while(temp2!=NULL){
+				if(hasEpsilon(temp2->rule_tokens)){
+					flg=1;
+				}
+				temp2=temp2->next;
 			}
-			temp2 = temp2->next;
-		}
-		
-		return first_list;
-		
+		}else if(!isEpsilon(temp->symbol))
+			flg = 0;
+		if(flg==0)
+			return 0;
+		temp=temp->next;
 	}
+	return 1;
 }
 
 
 
-void findFirst(){
-	SymbolList * temp = symbols;
+void findFollow(char const *first_nm,char const *follow_nm){
+
+	SymbolList * temp;
+	temp = symbols;
 	while(temp!=NULL){
-		if(temp->symbol->isTerminal==0){
-			if(temp->symbol->first==NULL){
-				SymbolList * first = findFirst_R(temp->symbol);
-				mergeList(&temp->symbol->first,first);
-			}
+		if(temp->symbol->isTerminal==1){
+			SymbolList *temp2 = NULL;
+			insertSymbol(&temp2,temp->symbol);
+			mergeList(&temp->symbol->first,temp2,1);
 		}
 		temp=temp->next;
 	}
 
-	// writing to a file
-	FILE *fptr = fopen("language/first.txt","w");
+	while(1){
+		int updated = 0;
+		temp = symbols;
+		while(temp!=NULL){
+			if(temp->symbol->isTerminal==0){
+				RuleList * temp2 = temp->symbol->rules;
+				while(temp2!=NULL){
+					SymbolList * temp3 = temp2->rule_tokens;
+					while(temp3!=NULL){
+						if(temp3 == temp2->rule_tokens || allEpsilon(temp2->rule_tokens,temp3))
+							updated |= mergeList(&temp->symbol->first,temp3->symbol->first,1);
+
+						if(temp3->symbol->isTerminal==0){
+							SymbolList * temp4 = temp3->next;
+							if(temp3->next==NULL)
+								updated |= mergeList(&temp3->symbol->follow,temp->symbol->follow,0);
+							while(temp4!=NULL){
+								if(temp3->next==NULL || allEpsilon(temp3->next,NULL)) 
+									updated |= mergeList(&temp3->symbol->follow,temp->symbol->follow,0);
+								if(temp4==temp3->next || allEpsilon(temp3->next,temp4))
+									updated |= mergeList(&temp3->symbol->follow,temp4->symbol->first,0);
+								temp4=temp4->next;
+							}
+						}
+						temp3=temp3->next;
+					}
+					temp2=temp2->next;
+				}
+			}
+			temp=temp->next;
+		}
+		if(!updated)
+			break;
+	}
+
+
+	FILE *fptr = fopen(first_nm,"w"),*fptr2 = fopen(follow_nm,"w");
 
 	temp = symbols;
 	while(temp!=NULL){
 		if(temp->symbol->isTerminal==0){
 			fprintf(fptr,"%s -> ",temp->symbol->value);
+			fprintf(fptr2,"%s -> ",temp->symbol->value);
 			SymbolList * temp2 = temp->symbol->first;
-			while(temp2->next!=NULL){
+			while(temp2!=NULL){
 				fprintf(fptr,"%s, ",temp2->symbol->value);
 				temp2=temp2->next;
 			}
-			fprintf(fptr,"%s",temp2->symbol->value);
 			fprintf(fptr,"\n");
+
+			temp2 = temp->symbol->follow;
+			while(temp2!=NULL){
+				fprintf(fptr2,"%s, ",temp2->symbol->value);
+				temp2=temp2->next;
+			}
+			fprintf(fptr2,"\n");
 		}
 		temp=temp->next;
 	}
 	fclose(fptr);
+	fclose(fptr2);
+
 
 }
 
 int main(int argc, char const *argv[])
 {
+	if(argc < 2){
+		printf("ERROR: Grammar file name not provided\n");
+		exit(0);
+	}
+	if(argc < 3){
+		printf("ERROR: Output file name not provided\n");
+		exit(0);
+	}
 	parseGrammar(argv[1]);
-	findFirst();
+	// findFirst(argv[2]);
+	findFollow(argv[2],argv[3]);
 	return 0;
 }
